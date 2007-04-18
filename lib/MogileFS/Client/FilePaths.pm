492627ac431c83089d463e4a8b7a7ac9650e46d6
hachi <hachi@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-04-18 04:16:49 +0000

    Getting ready for an initial release.
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@836 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- lib/MogileFS/Client/FilePaths.pm
+++ lib/MogileFS/Client/FilePaths.pm
@@ -46,7 +46,7 @@ All methods are inhereted and usable from the MogileFS::Client library, with onl
 use strict;
 use warnings;
 
-our $VERSION = '0.00_01';
+our $VERSION = '0.01';
 $VERSION = eval $VERSION;
 
 use base 'MogileFS::Client';
